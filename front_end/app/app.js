14c138d08f64b07bb2bb983989aade673218fa58
DLamarG <demondg8@gmail.com>
2025-02-24 18:56:50 -0600

    first commit

---
+++ front_end/app/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from Docker!</h1>
+      <p>This React app is served by Nginx inside a Docker container.</p>
+    </div>
+  );
+}
+
+export default App;
